3b983ad453aa488d2794da7d3e57ae4c8694c389
luke <luke.rast@gmail.com>
2024-03-09 11:21:29 -0500

    Fisher information progress: we have a reliable method and multiple other approaches to compare it to.

--- discriminationAnalysis.py
+++ discriminationAnalysis.py
@@ -7,32 +7,61 @@ import statsmodels.formula.api as smf
 
 from datageneration.stimulusGeneration import generateGrating
 
+from scipy.signal import savgol_filter
+
+
+# Smoothed derivatives
+def Fisher_smooth_fits(model, theta_start, theta_end,
+                       N_mean=10000, N_cov=1000, Samp_cov=1000):
+    """ Fisher information: use smoothing to get the derivative """
+    mean_angles = np.linspace(theta_start, theta_end, N_mean)
+    neural_activity = generate_samples(model, mean_angles)
+
+    derivs0 = savgol_filter(neural_activity[:, 0],
+                            window_length=700, polyorder=2, deriv=1,
+                            mode='wrap', delta=mean_angles[1]-mean_angles[0])
+    derivs1 = savgol_filter(neural_activity[:, 1],
+                            window_length=700, polyorder=2, deriv=1,
+                            mode='wrap', delta=mean_angles[1]-mean_angles[0])
+
+    derivs = np.array([derivs0, derivs1])
+
+    cov_angles = np.linspace(theta_start, theta_end, N_cov)
+
+    deriv_cov_ratio = N_mean // N_cov
+
+    FI = []
+    for i, angle in enumerate(cov_angles):
+        noisy_results = generate_samples(model,  Samp_cov*[angle])
+        invcov = np.linalg.inv(np.cov(noisy_results.T))
+
+        FI.append(derivs[:, deriv_cov_ratio*i] @ invcov @ derivs[:, deriv_cov_ratio*i])
+
+    return np.array(FI)
+
 
 # linear decoding
-def Fisher_discrimination(model, theta, dtheta=0.05, N_samples=1000,
-                          pixelDim=101, shotNoise=0.8, noiseVar=20
-                          ):
+def Fisher_discrimination(model, theta, dtheta=0.05, N_samples=1000):
     """ Measures linear Fisher information by training a linear decoder """
+    # test me 
 
-    def generate_samples(mid_point, dtheta, N, linear=True):
+    def sample_curve(mid_point, dtheta, N, linear=True):
         """ generate samples from the model """
         if linear:
             to_sample = np.linspace(mid_point-dtheta, mid_point+dtheta, N)
         else:
             to_sample = N//2*[mid_point - dtheta] + N//2*[mid_point + dtheta]
 
-        samples = model.forward(generateGrating(to_sample, pixelDim=pixelDim,
-                                                shotNoise=shotNoise, noiseVar=noiseVar)
-                                ).detach().numpy()
+        samples = generate_samples(model, to_sample)
 
         return pd.DataFrame({'stimulus': to_sample, 'r1': samples[:, 0],
                              'r2': samples[:, 1]})
 
-    train_resp = generate_samples(theta, 2*dtheta, N_samples, linear=True)
+    train_resp = sample_curve(theta, 2*dtheta, N_samples, linear=True)
     lin = smf.ols('stimulus ~ r1 + r2', data=train_resp)
     lin = lin.fit()
 
-    test_resp = generate_samples(theta, dtheta, N_samples, linear=False)
+    test_resp = sample_curve(theta, dtheta, N_samples, linear=False)
 
     o1 = lin.predict(test_resp[0:N_samples//2])
     o2 = lin.predict(test_resp[N_samples//2:])
@@ -41,18 +70,13 @@ def Fisher_discrimination(model, theta, dtheta=0.05, N_samples=1000,
 
 
 # direct sampling
-def Fisher_sampling(model, theta, dtheta=0.05, N_samples=1000, biasCorrect=True,
-                    pixelDim=101, shotNoise=0.8, noiseVar=20
-                    ):
+def Fisher_sampling(model, theta, dtheta=0.05, N_samples=1000, biasCorrect=True):
     """ Measure the linear Fisher by directly mean derivatives and covariances"""
-    def make_samples(angle, N):
-        return model.forward(generateGrating(N*[angle], pixelDim=pixelDim,
-                                             shotNoise=shotNoise, noiseVar=noiseVar)
-                             ).detach().numpy()
+    # test me
 
-    centered = make_samples(theta, N_samples)
-    bigger = make_samples(theta, N_samples)
-    smaller = make_samples(theta, N_samples)
+    centered = generate_samples(model, N_samples*[theta], N_samples)
+    bigger = generate_samples(model, N_samples*[theta+dtheta], N_samples)
+    smaller = generate_samples(model,  N_samples*[theta-dtheta], N_samples)
 
     covMat = np.cov(centered.T)
     tuningDeriv = (bigger.mean(0) - smaller.mean(0)) / (2*dtheta)
@@ -69,7 +93,17 @@ def Fisher_sampling(model, theta, dtheta=0.05, N_samples=1000, biasCorrect=True,
     return bcFisher
 
 
-# Direct decoding
+def generate_samples(model, thetas, pixelDim=101, shotNoise=0.8, noiseVar=20):
+    """ generate samples from the model """
+    samples = model.forward(generateGrating(thetas, pixelDim=pixelDim,
+                                            shotNoise=shotNoise, noiseVar=noiseVar)
+                            ).detach().numpy()
+    return samples
+
+
+########################### Direct decoding #########################
+#####
+
 def sensitivityIndex(model, angle, samples=1000, dtheta=0.01,
                      pixelDim=101, shotNoise=0.8, noiseVar=20):
     """Evaluate sensitivity to changes in stimulus"""
--- nonlinFisher.py
+++ nonlinFisher.py
@@ -7,10 +7,8 @@ from torch.utils.data import TensorDataset, DataLoader
 
 import pytorch_lightning as pl
 
-
 from basicModel import EstimateAngle
 from datageneration.stimulusGeneration import generateGrating
-
 
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning import Trainer
@@ -18,11 +16,61 @@ from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 
 import wandb
 
+from discriminationAnalysis import generate_samples
 
-class Discriminator(pl.LightningModule):
-    """Discriminator: trained for fine discrimination"""
+import pandas as pd
+import numpy as np
+
+
+def test_script():
+    """ Running our Fisher information test on a pretrained model """
+    ckpt = 'trainedParameters/Experiment2/380931818027565204/epoch=208-step=26752.ckpt'
+    model = EstimateAngle.load_from_checkpoint(ckpt)
+
+    stims = []
+    FIs = []
+    
+    for theta in [0., np.pi/4, np.pi/2, 3*np.pi/4]:
+        for rep in range(3):
+            FIs.append(Fisher_nlin_decoder(model, theta, 0.05).item())
+            stims.append(theta)
+
+            print(theta)
+
+    return pd.DataFrame({'Fisher': FIs, 'stimulus': stims})
+
+
+def Fisher_nlin_decoder(model, theta, dtheta, N_train=5000, N_val=1000, N_test=1000):
+    """EXPENSIVE!
+       First, train a nonlinear decoder, then use that to evaluate
+       the discrimination performance
+     """
+    discriminator = NeuralDecoder()
+    traindl, validdl = make_dataloader(theta, dtheta, trained_model=model,
+                                       N_train=N_train, N_val=N_val)
+
+    best_discriminator = run_training(discriminator, traindl, validdl, './FIruns')
+
+    thetas0 = N_test * [theta - dtheta/2]
+    thetas1 = N_test * [theta + dtheta/2]
+
+    left_decoding = best_discriminator(torch.tensor(
+                                         generate_samples(model, thetas0))
+                                       )
+    right_decoding = best_discriminator(torch.tensor(
+                                         generate_samples(model, thetas1))
+                                        )
+
+    FI = ((right_decoding.mean() - left_decoding.mean()) / dtheta)**2 /\
+         (0.5 * (right_decoding.var() + left_decoding.var()))
+
+    return FI
+
+
+class NeuralDecoder(pl.LightningModule):
+    """NeuralDecoder: trained for decoding neural activity data"""
     def __init__(self):
-        super(Discriminator, self).__init__()
+        super(NeuralDecoder, self).__init__()
         self.save_hyperparameters({'lr': 1E-3})
 
         self.discriminationNet = nn.Sequential(
@@ -30,21 +78,19 @@ class Discriminator(pl.LightningModule):
                     nn.ReLU(),
                     nn.Linear(5, 5),
                     nn.ReLU(),
-                    nn.Linear(5, 2),
-                    nn.Softmax(dim=1)
+                    nn.Linear(5, 1),
             )
-        self.lossFn = nn.BCELoss()
+        self.lossFn = nn.MSELoss()
 
     def forward(self, X):
-        likelihood = self.discriminationNet.forward(X)
-        ls, inds = torch.max(likelihood, dim=1)
+        angles = self.discriminationNet.forward(X)
 
-        return inds
+        return angles
 
     def training_step(self, batch, batchid=None):
         X, y = batch
 
-        prediction = self.discriminationNet.forward(X)
+        prediction = self.forward(X)
         loss = self.lossFn(prediction, y)
         self.log('trainLoss', loss)
         return loss
@@ -52,48 +98,48 @@ class Discriminator(pl.LightningModule):
     def validation_step(self, batch, batchid=None):
         X, y = batch
 
-        probabilities = self.discriminationNet.forward(X)
-        loss = self.lossFn(probabilities, y)
+        angles = self.forward(X)
+        loss = self.lossFn(angles, y)
 
-        predictions = self.forward(X)
-
-        accuracy = y[torch.arange(y.shape[0]), predictions].sum() / y.shape[0]
-
-        self.log('validAccuracy', accuracy)
         self.log('validLoss', loss)
-        return accuracy
+        return loss
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
 
 
-def make_dataloader(theta, dtheta, model_checkpoint, N_train=1000, N_val=500, batch_size=10):
+def make_dataloader(theta, dtheta, model_checkpoint=None, 
+                    trained_model=None,
+                    N_train=1000, N_val=500, batch_size=10):
     """ Generate samples from the models at hand """
 
-    trained_model = EstimateAngle.load_from_checkpoint(model_checkpoint)
+    if trained_model is None:
+        trained_model = EstimateAngle.load_from_checkpoint(model_checkpoint)
+
+    train_angles = torch.linspace(theta - dtheta, theta + dtheta, N_train) 
+
     X_train = trained_model.forward(
-                            generateGrating(N_train//2*[theta] + N_train//2*[theta + dtheta],
+                            generateGrating(train_angles,
                                             pixelDim=101, shotNoise=0.8, noiseVar=20)
                     ).detach()
-    y_train = nn.functional.one_hot(
-                        torch.tensor(N_train//2*[0.] + N_train//2*[1.]).long()
-                    ).float()
+    y_train = train_angles[:, None]
 
+    valid_angles = theta - dtheta + 2*dtheta*torch.rand(N_val)
     X_valid = trained_model.forward(
-                            generateGrating(N_val//2*[theta] + N_val//2*[theta + dtheta],
+                            generateGrating(valid_angles,
                                             pixelDim=101, shotNoise=0.8, noiseVar=20)
                     ).detach()
-    y_valid = nn.functional.one_hot(
-                        torch.tensor(N_val//2*[0.] + N_val//2*[1.]).long()
-                    ).float()
+    y_valid = valid_angles[:, None]
 
-    trainDL = DataLoader(TensorDataset(X_train, y_train), shuffle=True, batch_size=batch_size)
-    valDL = DataLoader(TensorDataset(X_valid, y_valid))
+    trainDL = DataLoader(TensorDataset(X_train, y_train), shuffle=True,
+                         batch_size=batch_size)
+    valDL = DataLoader(TensorDataset(X_valid, y_valid), batch_size=20, num_workers=1,
+                       persistent_workers=True)
 
     return trainDL, valDL
 
 
-def run_training(model, trainDL, validDL, directory, patience=200):
+def run_training(model, trainDL, validDL, directory, patience=50):
     """Simple training behavior with checkpointing"""
     wandb.init(reinit=True, project='Fisher Info')
     wandb_logger = WandbLogger()
@@ -108,7 +154,9 @@ def run_training(model, trainDL, validDL, directory, patience=200):
                                           )
 
     trainer = Trainer(logger=wandb_logger,
-                      max_epochs=5000,
+                      max_epochs=2000,
                       callbacks=[checkpoint_callback, earlystopping_callback]
                       )
     trainer.fit(model, trainDL, validDL)
+
+    return NeuralDecoder.load_from_checkpoint(checkpoint_callback.best_model_path)
