15f84c148c918315e7dd73aac212e7473653fbc5
luke <luke@MacBook-Pro.local>
2024-03-29 15:30:57 -0400

    experiment 4: non-uniform replicates

--- experiments/ex4_uniform_replicates.py
+++ experiments/ex4_uniform_replicates.py
@@ -1,7 +1,8 @@
-# baseline neural networks before and after training on uniform stimulus distributions
 
 import wandb
 import glob
+
+import numpy as np
 
 from basicModel import EstimateAngle
 from trainers import runEarlyStoppingTraining
@@ -36,5 +37,31 @@ def Experiment4_baselines():
         dummy.save_checkpoint(f'trainedParameters/Exp4/rep{rep}/pretrain.ckpt')
 
         runEarlyStoppingTraining(model, 
-                                 directory=f'trainedParameters/Exp4/rep{rep}/'
-                                 )
+                                 directory=f'trainedParameters/Exp4/rep{rep}/')
+                                 
+
+def Experiment4_concentrated():
+    """ Baseline trained models, non-uniform stimuli
+    """
+    defaultConfig = {
+                     'pixelDim': 101,
+                     'shotNoise': 0.8,
+                     'noiseVar': 20.,
+                     'dataSize': 512,
+                     'loc_tr': np.pi/2,
+                     'kappa_tr': 8.,
+                     'loc_val': np.pi/2,
+                     'kappa_val': 8.
+                     }
+
+    for rep in range(6):
+        """An individual training run for a sweep"""
+        wandb.init(reinit=True)
+        print('################ rep #############')
+        model = EstimateAngle(**defaultConfig,
+                              max_epochs=3000
+                              )
+
+        runEarlyStoppingTraining(model,
+                                 directory=f'trainedParameters/Exp4_conc/rep{rep}/')
+
