705be6dc4e1da030d70f2154b2743529d88d987e
lrast <luke.rast@gmail.com>
2024-09-09 13:56:19 -0400

    Developing methods for measuring Fisher information using network derivatives.

--- discriminationAnalysis.py
+++ discriminationAnalysis.py
@@ -6,8 +6,138 @@ import pandas as pd
 import statsmodels.formula.api as smf
 
 from datageneration.stimulusGeneration import generateGrating
+from datageneration.faces.rotated_olivetti import FaceDataset
 
 from scipy.signal import savgol_filter
+
+
+# taking advantage of neural network derivatives
+def Fisher_derivatives_faces_total(model, thetas, num_samples=1000, image_delta=0.05):
+    """ Direct evaluation of the Fisher information by taking derivatives of the neural
+        networks.
+    """
+    def point_Fisher(theta):
+        """ This could definitely be sped up by vectorization in a variety of dimensions """
+        I0 = FaceDataset(torch.zeros(80) + theta, split='test').images.contiguous()
+        I0.requires_grad = True
+
+        outputs = model.forward(I0.repeat((num_samples, 1, 1)).to(model.device)).cpu()
+        cov = outputs.T.cov()
+
+        mean_grad0 = torch.autograd.grad(outputs.mean(0)[0], I0, retain_graph=True)[0]
+        mean_grad1 = torch.autograd.grad(outputs.mean(0)[1], I0, retain_graph=True)[0]
+
+        cov_grad00 = torch.autograd.grad(cov[0,0], I0, retain_graph=True)[0]
+        cov_grad01 = torch.autograd.grad(cov[0,1], I0, retain_graph=True)[0]
+        cov_grad10 = torch.autograd.grad(cov[1,0], I0, retain_graph=True)[0]
+        cov_grad11 = torch.autograd.grad(cov[1,1], I0)[0]
+
+        cov = cov.detach()
+
+        plus_I0 = FaceDataset(torch.zeros(80) + theta + image_delta/2, split='test').images.contiguous()
+        minus_I0 = FaceDataset(torch.zeros(80) + theta - image_delta/2, split='test').images.contiguous()
+        image_deriv = (plus_I0 - minus_I0) / image_delta 
+
+        mean_deriv0 = (mean_grad0 * image_deriv).sum().item()
+        mean_deriv1 = (mean_grad1 * image_deriv).sum().item()
+        douts = torch.tensor([mean_deriv0, mean_deriv1])
+
+        cov_deriv00 = (cov_grad00 * image_deriv).sum().item()
+        cov_deriv01 = (cov_grad10 * image_deriv).sum().item()
+        cov_deriv10 = (cov_grad10 * image_deriv).sum().item()
+        cov_deriv11 = (cov_grad11 * image_deriv).sum().item()
+        dCov = torch.tensor([[cov_deriv00, cov_deriv01], [cov_deriv10, cov_deriv11]])
+
+        invcov = torch.linalg.inv(cov).cpu()
+
+        torch.mps.empty_cache()
+        return douts @ invcov @ douts + 0.5 * torch.trace(invcov @ dCov @ invcov @ dCov)
+
+    FIs = np.zeros(len(thetas))
+    for i, theta in enumerate(thetas):
+        FIs[i] = point_Fisher(theta)
+
+    return FIs
+
+
+
+def Fisher_derivatives_faces_linear(model, thetas, num_samples=1000, image_delta=0.05):
+    """ Direct evaluation of the Fisher information by taking derivatives of the neural
+        networks.
+
+        Written specifically for the face dataset.
+
+        In notebook 2.2 I explore how empirically this approach is substantially 
+        more statistically efficient that direct evaluation.
+    """
+    def point_Fisher(theta):
+        """ This could definitely be sped up by vectorization in a variety of dimensions """
+        I0 = FaceDataset(torch.zeros(80) + theta, split='test').images.contiguous()
+        I0.requires_grad = True
+
+        outputs = model.forward(I0.repeat((num_samples, 1, 1)).to(model.device)).cpu()
+        model_grad0 = torch.autograd.grad(outputs.mean(0)[0], I0, retain_graph=True)[0]
+        model_grad1 = torch.autograd.grad(outputs.mean(0)[1], I0)[0]
+
+        plus_I0 = FaceDataset(torch.zeros(80) + theta + image_delta/2, split='test').images.contiguous()
+        minus_I0 = FaceDataset(torch.zeros(80) + theta - image_delta/2, split='test').images.contiguous()
+        image_deriv = (plus_I0 - minus_I0) / image_delta 
+
+        full_deriv0 = (model_grad0 * image_deriv).sum().item()
+        full_deriv1 = (model_grad1 * image_deriv).sum().item()
+        douts = torch.tensor([full_deriv0, full_deriv1])
+
+        cov = outputs.T.cov().detach()
+        torch.mps.empty_cache()
+        return (douts @ torch.linalg.inv(cov) @ douts).item()
+
+    FIs = np.zeros(len(thetas))
+    for i, theta in enumerate(thetas):
+        FIs[i] = point_Fisher(theta)
+
+    return FIs
+
+
+def Fisher_derivatives_faces_covariance(model, thetas, num_samples=1000, image_delta=0.05):
+    """ The covariance term in the Fisher information
+        Evaluated by taking derivatives of the neural networks.
+
+        Written specifically for the face dataset.
+    """
+    def point_Fisher(theta):
+        """ This could definitely be sped up by vectorization in a variety of dimensions """
+        I0 = FaceDataset(torch.zeros(80) + theta, split='test').images.contiguous()
+        I0.requires_grad = True
+
+        outputs = model.forward(I0.repeat((num_samples, 1, 1)).to(model.device)).cpu()
+        cov = outputs.T.cov()
+        model_grad00 = torch.autograd.grad(cov[0,0], I0, retain_graph=True)[0]
+        model_grad01 = torch.autograd.grad(cov[0,1], I0, retain_graph=True)[0]
+        model_grad10 = torch.autograd.grad(cov[1,0], I0, retain_graph=True)[0]
+        model_grad11 = torch.autograd.grad(cov[1,1], I0)[0]
+
+        plus_I0 = FaceDataset(torch.zeros(80) + theta + image_delta/2, split='test').images.contiguous()
+        minus_I0 = FaceDataset(torch.zeros(80) + theta - image_delta/2, split='test').images.contiguous()
+        image_deriv = (plus_I0 - minus_I0) / image_delta 
+
+        full_deriv00 = (model_grad00 * image_deriv).sum().item()
+        full_deriv01 = (model_grad10 * image_deriv).sum().item()
+        full_deriv10 = (model_grad10 * image_deriv).sum().item()
+        full_deriv11 = (model_grad11 * image_deriv).sum().item()
+
+        dCov = torch.tensor([[full_deriv00, full_deriv01], [full_deriv10, full_deriv11]])
+        cov = cov.detach()
+        invcov = torch.linalg.inv(cov)
+        torch.mps.empty_cache()
+        return 0.5 * torch.trace(invcov @ dCov @ invcov @ dCov)
+
+    FIs = np.zeros(len(thetas))
+    for i, theta in enumerate(thetas):
+        FIs[i] = point_Fisher(theta)
+
+    return FIs
+
+
 
 
 # Smoothed derivatives
@@ -32,7 +162,7 @@ def Fisher_smooth_fits(model, theta_start, theta_end,
 
     FI = []
     for i, angle in enumerate(cov_angles):
-        noisy_results = generate_samples(model,  Samp_cov*[angle])
+        noisy_results = generate_samples(model, Samp_cov*[angle])
         invcov = np.linalg.inv(np.cov(noisy_results.T))
 
         FI.append(derivs[:, deriv_cov_ratio*i] @ invcov @ derivs[:, deriv_cov_ratio*i])
@@ -101,8 +231,7 @@ def generate_samples(model, thetas, pixelDim=101, shotNoise=0.8, noiseVar=20):
     return samples
 
 
-########################### Direct decoding #########################
-#####
+########################### Direct decoding ##############################
 
 def sensitivityIndex(model, angle, samples=1000, dtheta=0.01,
                      pixelDim=101, shotNoise=0.8, noiseVar=20):
