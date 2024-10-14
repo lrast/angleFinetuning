b02ed8eaf19b58c06d2a13b85c450fdc8e5b38d8
lrast <luke.rast@gmail.com>
2024-10-14 17:14:59 -0400

    Results of linear and circular sweeps: adaptation to probability that tracks density with a weak dependence on loss function

--- experiments/ex2.0_exploring_more_architectures.py
+++ experiments/ex2.0_exploring_more_architectures.py
@@ -52,3 +52,57 @@ def Exp22_sweep_linear_and_circular():
                                f'trainedParameters/Exp2.2/{loss_pair[0]}_loss/{encoding}_enc/{distribution_pair[0]}/rep{rep}',
                                save_best_train=True
                                )
+
+
+def Exp23_sweep_3():
+    """
+        I need more aggressive loss function changes to detect et 
+    """
+    losses = [('square', lambda x: x**2),
+              ('quart', lambda x: x**4)
+              ]
+    dists = {
+            'linear': [('uniform', uniform(-torch.pi/2, torch.pi).rvs),
+                       ('concentrated', norm(torch.pi/4, 0.2).rvs)
+                       ],
+            'circular': [('uniform', uniform(-torch.pi, 2*torch.pi).rvs),
+                         ('concentrated', vonmises(torch.pi/2, 2.).rvs)
+                         ]
+            }
+
+    for loss_pair in losses:
+        for rep in range(3):
+            for encoding in ['linear', 'circular']:
+                for distribution_pair in dists[encoding]:
+                    model = FaceAngle(loss_pair, distribution_pair, encoding)
+                    trainModel(model,
+                               f'trainedParameters/Exp2.2/{loss_pair[0]}_loss/{encoding}_enc/{distribution_pair[0]}/rep{rep}',
+                               save_best_train=True
+                               )
+
+
+def Exp23_sweep_losses():
+    """
+        I need more aggressive loss function changes to detect et 
+    """
+    losses = [('root4', lambda x: x**0.25),
+              ('log', torch.log)
+              ]
+    dists = {
+            'linear': [('uniform', uniform(-torch.pi/2, torch.pi).rvs),
+                       ('concentrated', norm(torch.pi/4, 0.2).rvs)
+                       ],
+            'circular': [('uniform', uniform(-torch.pi, 2*torch.pi).rvs),
+                         ('concentrated', vonmises(torch.pi/2, 2.).rvs)
+                         ]
+            }
+
+    for loss_pair in losses:
+        for rep in range(3):
+            for encoding in ['linear']:
+                for distribution_pair in dists[encoding]:
+                    model = FaceAngle(loss_pair, distribution_pair, encoding)
+                    trainModel(model,
+                               f'trainedParameters/Exp2.2/{loss_pair[0]}_loss/{encoding}_enc/{distribution_pair[0]}/rep{rep}',
+                               save_best_train=True
+                               )
